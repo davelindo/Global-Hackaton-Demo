c663e95598a0560ccea027c32b6820f3f7c702d8
akuma <leo.low.chan@gmail.com>
2018-10-21 21:05:52 -0700

    add react proxy

--- react-express-proxy/app.js
+++ react-express-proxy/app.js
@@ -1,9 +1,15 @@
 var express  = require('express');
+var openAMAgent = require('openam-agent');
 var app      = express();
+agent = openamAgent({openamUrl: 'http://35.203.155.12:8080/lab126-auth'});
 var httpProxy = require('http-proxy');
 var apiProxy = httpProxy.createProxyServer();
-var serverOne = 'http://localhost:3000';
- 
+var serverOne = 'http://35.197.24.127:5000';
+
+app.get('/', agent.shield(openamAgent.cookieShield({getProfiles: true})), function (req, res) {
+    console.log(req.session.userName);
+    });
+    
 app.all("/*", function(req, res) {
     console.log('redirecting to Server1');
     apiProxy.web(req, res, {target: serverOne});
